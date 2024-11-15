e245062ecc369cf969b43c367c16e7fb84d03a00
Daniel Garcia <daniieelgs@gmail.com>
2024-11-15 17:41:52 +0100

    fix dict store

--- scripts/modals/SettingsModal.js
+++ scripts/modals/SettingsModal.js
@@ -411,8 +411,8 @@ class SettingsModal extends Modal {
                             label.style.gap = "2px";
                         }else if(settings.type == "Dict") {
                             let value = (typeConfig.vendor == vendor.value ? typeConfig.settings[settings.field] : null) || (settings.default == "None" ? "" : settings.default);
-                            if(typeof value === 'object') value = JSON.stringify(value, null, 2);
-                            input.value = value;
+                            if(typeof value === 'object') input.value = JSON.stringify(value, null, 2);
+                            else input.value = value;
                             input.placeholder = settings.description;
                         }else {
                             input.value = (typeConfig.vendor == vendor.value ? typeConfig.settings[settings.field] : null) || (settings.default == "None" ? "" : settings.default);
